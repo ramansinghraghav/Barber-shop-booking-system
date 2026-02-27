cbac8c526ddf021c454b99e11b74bfb24c6bee95
RAMAN SINGH RAGHAV <2203051240081@paruluniversity.ac.in>
2026-02-27 16:17:09 +0530

    done

--- Barber-Frontend/src/App.jsx
+++ Barber-Frontend/src/App.jsx
@@ -1,17 +1,35 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import CustomerDashboard from "./pages/CustomerDashboard";
 import BarberDashboard from "./pages/BarberDashboard";
 
 function App() {
+  const token = localStorage.getItem("access_token");
+  const user = JSON.parse(localStorage.getItem("user") || "null");
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Signup />} />
+
+        {/* Default route */}
+        <Route
+          path="/"
+          element={
+            token
+              ? user?.role === "customer"
+                ? <Navigate to="/customer" />
+                : <Navigate to="/barber" />
+              : <Navigate to="/login" />
+          }
+        />
+
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
         <Route path="/customer" element={<CustomerDashboard />} />
         <Route path="/barber" element={<BarberDashboard />} />
+
       </Routes>
     </BrowserRouter>
   );
